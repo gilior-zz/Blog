c81104eecd0c603c4290d862406e7aa758287b15
lior gishry <liorgish@gmail.com>
2020-09-28 12:11:50 +0300

    base name (if app served from sub directory)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename='/'>
         <div className="App">
           <Blog />
         </div>
